68458f0f5f1e227c3a8dbc9416079fa5bfb6ff83
Joe Turner <joe@agavemountain.com>
2018-12-07 16:23:00 -0600

    Add global header file with plenty of doxygen goodness.
    Added a global header file that contains a lot of verbose documentation.

---
+++ monkeylib/include/monkeylib/core/monkey-hammer.h
@@ -0,0 +1,83 @@
+/**
+ *  @file: monkey-hammer.h
+ *
+ *  Copyright (C) 2018  Joe Turner <joe@agavemountain.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+/*! \mainpage
+
+ \section intro Introduction
+
+ Welcome to the Monkey Hammer suite of code tools.
+
+ Over the years, I have written hackish-quick-and-dirty utilities to generate
+ code and automate various development tasks.  This project is a collection
+ of those tools, in an combined and re-factored form.
+
+ \tableofcontents
+
+ \section Requirements
+
+ - Doxygen 1.8.0+ (for markdown support) and GraphViz.
+ - GNU GCC toolchain
+ - Git (obviously)
+ - CPPUNIT (for unit tests)
+
+\section make Building and Installing
+
+ \code
+ $ mkdir build
+ $ cmake ..
+ $ make;
+ $ sudo make install
+ \endcode
+
+\section uninstall Uninstalling
+
+\code
+$ sudo make uninstall
+\endcode
+
+ \section Generating Documentation
+
+ This project uses doxygen to generate the documentation.  To generate
+ this documentation, you must have doxygen installed.  Simple execute doxygen:
+ \code
+ $ make doc
+ \endcode
+
+
+ \section Running Unit Tests
+
+ You can run unit tests (if cppunit is installed on your development workstation):
+
+ \code
+ $ ctest -VV
+ \endcode
+
+
+*/
+
+//! \brief Monkey Hammer namespace
+//!
+//! Namespaces allow us to group named entities that otherwise would
+//! have global scope into narrower scopes, giving them namespace scope.
+//! This allows organizing the elements of programs into different logical
+//! scopes referred to by names.
+//!
+//! Per good standards, all of the Monkey Hammer code is contained within a
+//! namespace.
