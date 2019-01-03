dd118acc5da6b4f7fc9a245f5bb755c278c10ea1
Joe Turner <joe@agavemountain.com>
2019-01-03 16:52:27 -0600

    stubbing in to makefiles.

---
+++ codemonkey/source/codemonkey.cpp
@@ -0,0 +1,31 @@
+/**
+ *  @file: codemonkey.cpp
+ *
+ * This utility makes it easy to generate a take a text file
+ * and turn it into a character array so it can be embedded
+ * into an compiled executable.
+ *
+ *  Copyright (C) 2018  Joe Turner <joe@agavemountain.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+#include <string>
+#include <fstream>
+
+//! Program Entry Point
+int main(int ac, char **av) {
+
+    return 0;
+}
